a477becda139eb1c1ce40a73ad599943ff376b8a
Matthew Garrod <mgarrod193@gmail.com>
2024-04-07 14:29:12 +0100

    Added tilemap and simple level, physics for map tiles and background music

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -450.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
