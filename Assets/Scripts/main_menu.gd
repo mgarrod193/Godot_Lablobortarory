c305b870fe695b201ad9d404c655a9bcf55e0ac1
mgarrod <45399726+mgarrod193@users.noreply.github.com>
2024-06-27 23:55:05 +0100

    feature b

--- Assets/Scripts/main_menu.gd
+++ Assets/Scripts/main_menu.gd
@@ -4,6 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Hello")
+	print("feature b")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
